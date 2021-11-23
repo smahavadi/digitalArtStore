67de18c4cae8a27c4892766b961550bce356eebb
[Cherny Devireddy] <[cdevired@uncc.edu]>
2021-11-23 14:16:01 -0500

    final delete

--- microservice-frontend-master/cart.js
+++ microservice-frontend-master/cart.js
@@ -52,37 +52,12 @@ function getCart($email) {
 //Cherny Devireddy
 function deleteItem($id) {
     $.ajax({
-        url: Url + 'RemoveProduct',
+        url: Url + 'Cart/' + $id,
         type: 'delete',
         dataType: 'json',
-        data: {"product_id":$id},
         contentType: 'text/plain',
         success: function (data) {
-            list = '';
-            listAdd = '';
-            itemCount = 0;
-            totalPrice = 0;
-
-            $.each(data['data']['List'], function (i, item) {
-                listAdd = '<div class="row main align-items-center">\n' +
-                    '                        <div class="col-2"><img class="img-fluid" src="' + item['image'] + '"></div>\n' +
-                    '                        <div class="col">\n' +
-                    '                            <div class="row text-muted">' + item['operating_system'] + '</div>\n' +
-                    '                            <div class="row">' + item['title'] + '</div>\n' +
-                    '                        </div>\n' +
-                    '                        <div class="col"> <a class="border">1</a></div>\n' +
-                    '                        <div class="col">&dollar; ' + item['money_price'] + ' <a onclick="deleteItem(' + item['id'] + ')" type="button">&#10005;</a></div>\n' +
-                    '                    </div>';
-                list = list + listAdd;
-                itemCount++;
-                totalPrice += parseInt(item['money_price']);
-            });
-
-            $('#cart-list').html(list);
-            $('#item-count').html(itemCount + ' items');
-            $('#item-total').html(itemCount + ' items');
-            $('#item-price').html('&dollar; ' + totalPrice);
-
+            getCart(email);
         },
         error: function (data) {
             alert("Error while fetching data.");
